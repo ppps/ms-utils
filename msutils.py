6373d008acdef1016573857842e576f5111eefc1
Rob Wells <rob@robjwells.com>
2017-01-05 20:07:03 +0000

    add Page class

--- msutils.py
+++ msutils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
+from pathlib import Path
 import re
-
 
 def parse_page_name(name):
     """Extract page, section and date from a filename
@@ -38,3 +38,50 @@ def parse_page_name(name):
     return {'pages': pages,
             'section': groups['section'],
             'date': date}
+
+
+class Page(object):
+    """Represents a page file on disk.
+
+    Can be used for both InDesign (.indd) and PDF files.
+    """
+
+    _page_name_regex = re.compile('''\
+    ^
+    (?P<first_page>  \d+)
+    (?: -
+        (?P<second_page> \d+)
+    )?
+    [-_ ]+
+    (?P<section> .+? )
+    [-_ ]+
+    (?P<date> \d{6} )
+    \.
+    (?P<type> \w+ )
+    ''', flags=re.VERBOSE)
+
+    _page_date_format = '%d%m%y'
+
+    def __init__(self, page_path: Path):
+        """Set up Page from a path to a file on disk
+
+        page_path:  pathlib.Path object
+
+        The page stored at page_path should be named according to the
+        usual Morning Star convention, described in this class's
+        page_name_regex pattern.
+        """
+        regex_match = self._page_name_regex.match(page_path.name)
+        if not regex_match:
+            raise ValueError('Page does not have a valid name')
+
+        pages = [regex_match['first_page']]
+        if regex_match['second_page'] is not None:
+            pages.append(regex_match['second_page'])
+        self.pages = tuple(map(int, pages))
+
+
+        self._date = datetime.strptime(regex_match['date'],
+                                       self._page_date_format)
+
+        self._section = regex_match['section']
