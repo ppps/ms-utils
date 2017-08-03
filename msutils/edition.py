ab2e6319735d03f7262226fe9e17a3b18f62d3e1
Rob Wells <robjwells@users.noreply.github.com>
2017-08-03 18:48:47 +0100

    Break into modules, add (prob broken) edition funcs

---
+++ msutils/edition.py
@@ -0,0 +1,78 @@
+from pathlib import Path
+
+from .page import Page
+
+PAGES_ROOT = Path('~/Server/Pages/').expanduser()
+PAGES_TEMPLATE = '{0:%Y-%m-%d %A %b %-d}'
+PDFS_TEMPLATE = '{0:PDFs %d%m%}'
+WEB_PDFS_TEMPLATE = '{0:E-edition PDFs %d%m%y}'
+
+
+class NoEditionError(Exception):
+    """No edition can be found for the given date"""
+    pass
+
+
+def edition_dir(date):
+    """Return path to date's edition directory
+    
+    Raises NoEditionError if an edition can't be
+    found in the expected locations for date.
+    """
+    ed_dir = PAGES_ROOT.joinpath(
+        PAGES_TEMPLATE.format(date)
+        )
+    if ed_dir.exists():
+        return ed_dir.resolve()
+    else:
+        raise NoEditionError(f'Cannot find edition for {date:%Y-%m-%d}')
+
+
+def _edition_press_pdfs_dir(date):
+    """Return path to pre-press PDFs directory for date's edition"""
+    ed_dir = edition_dir(date)
+    return ed_dir.joinpath(PDFS_TEMPLATE.format(date))
+
+
+def _edition_web_pdfs_dir(date):
+    """Return path to pre-press PDFs directory for date's edition"""
+    ed_dir = edition_dir(date)
+    return ed_dir.joinpath(WEB_PDFS_TEMPLATE.format(date))
+
+
+def _paths_to_pages(paths):
+    """Yield Pages from Paths, handling exceptions from non-Pages"""
+    for p in paths:
+        try: 
+            yield Page(p)
+        except ValueError:
+            continue
+
+
+def _parse_pdfs_dir(path):
+    """List Page-acceptable PDFs in path"""
+    # PDFs directory may not exist yet, even if the edition does
+    if not path.exists():
+        return []
+    all_pdfs = [p for p in path.iterdir() if p.suffix = '.pdf']
+    return sorted(_paths_to_pages(all_pdfs))
+
+
+def edition_indd_files(date):
+    """List InDesign Pages for date's edition"""
+    ed = edition_dir(date)
+    all_indd = [p for p in ed.iterdir() if p.suffix == '.indd']
+    return sorted(_paths_to_pages(all_indd))
+            
+
+def edition_press_pdfs(date):
+    """List pre-press PDFs for date's edition"""
+    pdfs_dir = _edition_press_pdfs_dir(date)
+    return _parse_pdfs_dir(pdfs_dir)
+
+
+def edition_web_pdfs(date):
+    """List low-quality PDFs for date's edition"""
+    pdfs_dir = _edition_web_pdfs_dir(date)
+    return _parse_pdfs_dir(pdfs_dir)
+
---
+++ msutils/page.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from pathlib import Path
+import re
+
+
+class Page(object):
+    """Represents a page file on disk.
+
+    Can be used for both InDesign (.indd) and PDF files.
+    """
+
+    _page_name_regex = re.compile('''\
+    ^
+    (?P<prefix> [a-zA-Z]{0,1} )
+    (?P<first_page>  \d+)
+    (?: -
+        (?P<second_page> \d+)
+    )?
+    [-_ ]*
+    (?P<section> \D+? )
+    [-_ ]*
+    (?P<date> \d{6} | \d{2}-\d{2}-\d{2,4} )
+    \.
+    (?P<type> \w+ )
+    $
+    ''', flags=re.VERBOSE)
+
+    _page_date_format = '%d%m%y'
+
+    _external_template = 'MS_{date:%Y}_{date:%m}_{date:%d}_{page:03}.{type}'
+
+    def __init__(self, page_path: Path):
+        """Set up Page from a path to a file on disk
+
+        page_path:  pathlib.Path object
+
+        The page stored at page_path should be named according to the
+        usual Morning Star convention, described in this class's
+        page_name_regex pattern.
+        """
+        regex_match = self._page_name_regex.match(page_path.name)
+        if not regex_match:
+            raise ValueError(f'{page_path.name} is an invalid filename')
+
+        self.path = page_path.expanduser()
+
+        pages = [regex_match['first_page']]
+        if regex_match['second_page'] is not None:
+            pages.append(regex_match['second_page'])
+        self.pages = tuple(map(int, pages))
+
+        date_match = regex_match['date'].replace('-', '')
+        if len(date_match) == 8:
+            date_match = date_match[:-4] + date_match[-2:]
+
+        self.date = datetime.strptime(date_match, self._page_date_format)
+
+        self.prefix = regex_match['prefix']
+        self.section = regex_match['section']
+        self.type = regex_match['type'].lower()
+
+    def __str__(self):
+        """Return the name of the underlying file"""
+        return self.path.name
+
+    def __repr__(self):
+        """Return string Page(Path(…))"""
+        return '{0}({1})'.format(self.__class__.__name__,
+                                 repr(self.path))
+
+    def external_name(self):
+        """Returns string used outside the Star to identify the page
+
+        Raises ValueError if called on instances representing a file
+        representing more than a single page.
+        """
+        if len(self.pages) > 1:
+            raise ValueError(
+                'external_name is invalid for Page instances that represent'
+                'multiple pages'
+                )
+        return self._external_template.format(
+            date=self.date,
+            page=self.pages[0],
+            type=self.type)
+
