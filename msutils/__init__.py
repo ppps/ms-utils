20d94087b76e4d4d3233a8b1deae344c2e3cd530
Rob Wells <rob@robjwells.com>
2017-08-05 17:57:06 +0100

    Import uploading functions in msutils.__init__

--- msutils/__init__.py
+++ msutils/__init__.py
@@ -1,5 +1,6 @@
 from .page import Page
 from .edition import (
     edition_dir, NoEditionError,
-    edition_indd_files, edition_press_pdfs, edition_web_pdfs
-    )
+    edition_indd_files, edition_press_pdfs, edition_web_pdfs)
+from .uploading import (
+    send_pages_ftp, send_pages_sftp)
