984d88368b38541b0a2a5d9ef6b2478549a9e046
Rob Wells <rob@robjwells.com>
2017-08-05 17:56:06 +0100

    Uploading: implement FTP and SFTP functions

--- msutils/uploading.py
+++ msutils/uploading.py
@@ -23,7 +23,19 @@ def send_pages_ftp(pages, host, user, password='',
     by calling its external_name method. If rename is False the
     page's current filename will be used.
     """
-    pass
+    with ftplib.FTP(host=host, user=user, passwd=password) as server:
+        if path is not None:
+            server.cwd(path)
+        for page in pages:
+            if rename:
+                new_name = page.external_name()
+            else:
+                new_name = page.path.name
+            with open(page.path, 'rb') as page_file:
+                print(server.storbinary)
+                server.storbinary(
+                    f'STOR {new_name}',
+                    page_file)
 
 
 def send_pages_sftp(pages, host, user, password=None,
@@ -33,7 +45,7 @@ def send_pages_sftp(pages, host, user, password=None,
     pages: [Page],
     host: str,
     user: str,
-    passwd: str = None,
+    password: str = None,
     port: int = 22,
     path: str = None,
     rename: bool = True
@@ -51,4 +63,31 @@ def send_pages_sftp(pages, host, user, password=None,
     by calling its external_name method. If rename is False the
     page's current filename will be used.
     """
-    pass
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    if password is None:
+        ssh_client.load_system_host_keys()
+    try:
+        ssh_client.connect(hostname=host, port=port, username=user,
+                           password=password)
+    except paramiko.AuthenticationException as e:
+        print('Authentication failed:', e)
+        return
+    except paramiko.BadHostKeyException as e:
+        print('Host key error:', e)
+        return
+    except paramiko.SSHException as e:
+        print('SSH error prevented connection:', e)
+        return
+
+    with ssh_client.open_sftp() as server:
+        if path is not None:
+            server.chdir(path)
+        for page in pages:
+            if rename:
+                new_name = page.external_name()
+            else:
+                new_name = page.path.name
+            server.put(page.path, new_name)
+
+    ssh_client.close()
