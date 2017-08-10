5ef9693eea03941c87155a344129339d67a35d91
Rob Wells <rob@robjwells.com>
2017-08-10 15:29:54 +0100

    Edition: allow local/remote Pages access, support archives

--- msutils/__init__.py
+++ msutils/__init__.py
@@ -1,6 +1,6 @@
 from .page import Page
 from .edition import (
-    edition_dir, NoEditionError,
+    edition_dir, NoEditionError, NoEditionStoresError,
     edition_indd_files, edition_press_pdfs, edition_web_pdfs,
     directory_indd_files, directory_pdfs)
 from .uploading import (
--- msutils/edition.py
+++ msutils/edition.py
@@ -3,14 +3,48 @@ from pathlib import Path
 from .page import Page
 
 PAGES_ROOT = Path('~/Server/Pages/').expanduser()
-PAGES_TEMPLATE = '{0:%Y-%m-%d %A %b %-d}'
+CURRENT_EDITION_TEMPLATE = '{0:%Y-%m-%d %A %b %-d}'
+ARCHIVE_EDITION_TEMPLATE = '{0:%Y/%m %B/%Y-%m-%d %A}'
 PRESS_PDFS_TEMPLATE = '{0:PDFs %d%m%y}'
 WEB_PDFS_TEMPLATE = '{0:E-edition PDFs %d%m%y}'
+
+EDITION_STORES = [
+    (Path('~/Server/Pages').expanduser(), CURRENT_EDITION_TEMPLATE),
+    (Path('/Volumes/MS-T4-Archive-2002-2016'), ARCHIVE_EDITION_TEMPLATE),
+    (Path('/Volumes/MS-T4-Archive-Since-2017'), ARCHIVE_EDITION_TEMPLATE),
+    (Path('/Volumes/Server/Pages'), CURRENT_EDITION_TEMPLATE),
+    (Path('/Volumes/Archive-2002-2016'), ARCHIVE_EDITION_TEMPLATE),
+    (Path('/Volumes/Archive-Since-2017'), ARCHIVE_EDITION_TEMPLATE),
+    ]
 
 
 class NoEditionError(Exception):
     """No edition can be found for the given date"""
     pass
+
+
+class NoEditionStoresError(Exception):
+    """No edition stores are present on the system"""
+    pass
+
+
+def _fetch_stores():
+    """Return paths to edition stores along with path templates
+
+    Result is a list of tuples:
+        [(path: Path, path_template: str)]
+
+    Template is returned because of differences in where editions are
+    stored when considered 'current' and when moved to the archive.
+    """
+    present_stores = [(path, ed_template)
+                      for (path, ed_template) in EDITION_STORES
+                      if path.exists()]
+    if present_stores:
+        return present_stores
+    else:
+        raise NoEditionStoresError(
+            'No edition stores are connected, either current or archive')
 
 
 def edition_dir(date):
@@ -19,11 +53,11 @@ def edition_dir(date):
     Raises NoEditionError if an edition can't be
     found in the expected locations for date.
     """
-    ed_dir = PAGES_ROOT.joinpath(
-        PAGES_TEMPLATE.format(date)
-        )
-    if ed_dir.exists():
-        return ed_dir.resolve()
+    present_stores = _fetch_stores()
+    for store, path_template in present_stores:
+        candidate = store.joinpath(path_template.format(date))
+        if candidate.exists():
+            return candidate
     else:
         raise NoEditionError(f'Cannot find edition for {date:%Y-%m-%d}')
 
